a4e01dc82e4be189b2260c94c592843d2c70d2e6
jayjang0209 <jayjang0209@gmail.com>
2024-01-19 21:58:18 -0800

    add teacher routes

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,40 +1,51 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { Layout, Compass } from "lucide-react"
-import SideBarItem from './sidebar-item'
+import React from "react";
+import { Layout, Compass, List, BarChart } from "lucide-react";
+import SideBarItem from "./sidebar-item";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
-    {
-        path: "/",
-        name: "Dashboard",
-        icon: Layout,
-    },
-    {
-        path: "/search",
-        name: "Browse",
-        icon: Compass,
-    },
-]
+  {
+    path: "/",
+    name: "Dashboard",
+    icon: Layout,
+  },
+  {
+    path: "/search",
+    name: "Browse",
+    icon: Compass,
+  },
+];
 
+const teacherRoutes = [
+  {
+    path: "/teacher/courses",
+    name: "Courses",
+    icon: List,
+  },
+  {
+    path: "/teacher/analytics",
+    name: "Analytics",
+    icon: BarChart,
+  },
+];
 
 export default function SidebarRoutes() {
-  const routes = guestRoutes;
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.includes("/teacher");
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
-  
-    return (
-    <div className='flex flex-col w-full gap-y-2'>
-    {
-        routes.map((route, index) => (
-            <SideBarItem
-                key={index}
-                path={route.path}
-                name={route.name}
-                icon={route.icon}
-            />
-        ))
-    }
-
+  return (
+    <div className="flex flex-col w-full gap-y-2">
+      {routes.map((route, index) => (
+        <SideBarItem
+          key={index}
+          path={route.path}
+          name={route.name}
+          icon={route.icon}
+        />
+      ))}
     </div>
-  )
+  );
 }
