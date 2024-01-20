3ee5e7cb1750848967d6da2d3d02cf4427fbd249
jayjang0209 <jayjang0209@gmail.com>
2024-01-19 21:47:37 -0800

    add Teacher mode button

--- app/(dashboard)/_components/navbar-routes.tsx
+++ app/(dashboard)/_components/navbar-routes.tsx
@@ -1,12 +1,35 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React from "react";
 import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export default function NavbarRoutes() {
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isCoursePage = pathname?.startsWith("/course");
+  const isSearchPage = pathname === "/search";
+
   return (
-    <div>
-        <UserButton />
-  </div>
-  )
+    <div className="flex gap-x-2 ml-auth">
+      {isTeacherPage ? 
+        <Button
+        ><LogOut className="h-4 w-4 mr-2" /> Log Out
+        </Button> :
+        (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Teacher Mode
+            </Button>
+          </Link>
+        ) 
+      }
+      <UserButton 
+        afterSignOutUrl="/"
+      />
+    </div>
+  );
 }
