255e945512d46712bcda354a5270f0c32df7b153
jayjang0209 <jayjang0209@gmail.com>
2024-01-20 14:14:27 -0800

    implement title description forms

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,15 +1,65 @@
 import React from "react";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { IconBadge } from "@/components/icon-badge";
+import { LayoutDashboard } from "lucide-react";
+import TitleForm from "./_components/title-form";
+import DescriptionForm from "./_components/description-form";
 
-export default function CourseIdPage({
+export default async function CourseIdPage({
   params,
 }: {
   params: {
     courseId: string;
   };
 }) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+
+  if (!course) {
+    redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+  const completionText = `${completedFields}/${totalFields} fields completed`;
+
   return (
-    <div>
-      <h1>Course ID: {params.courseId}</h1>
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-dedium">Course Setup</h1>
+          <span className="text-sm text-slate-700">{completionText}</span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-2xl font-medium">Course Information</h2>
+          </div>
+          <TitleForm initialData={course} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
