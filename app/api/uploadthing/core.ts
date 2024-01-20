ac3492dd30b5d0bd99d8601478634253cc51baa4
jayjang0209 <jayjang0209@gmail.com>
2024-01-20 14:57:59 -0800

    implement fileuploader

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,25 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { auth } from "@clerk/nextjs";
+
+const f = createUploadthing();
+
+const handleAuth = () => {
+  const { userId } = auth();
+  if (!userId) throw new Error("Unauthorized");
+  return { userId };
+};
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  courseImage: f({ image: { maxFileSize: "2MB", maxFileCount: 1 } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  courseAttachment: f(["text", "image", "video", "audio", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  chapterVideo: f({ video: { maxFileSize: "512MB", maxFileCount: 1 } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
